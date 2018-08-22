d46791ae0d87fdf51b63f7f3e52aab2b0d6ffdd6
Jose Garcia <josegarciamanez@gmail.com>
2018-08-22 17:51:07 +0200

    title

--- portfolio/.vuepress/config.js
+++ portfolio/.vuepress/config.js
@@ -1,23 +1,23 @@
 module.exports = {
-  title: "Acme",
-  description: "creative agency",
-  base: "/",
-  themeConfig: {
-    logo: "/upload/logo.svg",
-    footer: "Ⓒ Acme",
-    nav: [
-      { text: "Works", link: "/", position: "left", external: false },
-      { text: "Instagram", link: "https://www.instagram.com/its.nwa/", position: "left", external: true },
-      { text: "Say hi!", link: "mailto:sayhi@mydomain.com", position: "right", external: true },
-      { text: 'Journal', link: '/journal/', position: 'right', external: false },
-    ]
-  },
-  head: [
-    ['link', { rel: "icon", href: "favicon-32x32.png" }]
-  ],
-  markdown: {
-    anchor: {
-      permalink: false
+    title: "José García Máñez",
+    description: "Web Developer",
+    base: "/",
+    themeConfig: {
+        logo: "/upload/logo.svg",
+        footer: "Ⓒ josegarciamanez",
+        nav: [
+            { text: "Works", link: "/", position: "left", external: false },
+            { text: "Instagram", link: "https://www.instagram.com/its.nwa/", position: "left", external: true },
+            { text: "Say hi!", link: "mailto:sayhi@mydomain.com", position: "right", external: true },
+            { text: 'Journal', link: '/journal/', position: 'right', external: false },
+        ]
+    },
+    head: [
+        ['link', { rel: "icon", href: "favicon-32x32.png" }]
+    ],
+    markdown: {
+        anchor: {
+            permalink: false
+        }
     }
-  }
 };
